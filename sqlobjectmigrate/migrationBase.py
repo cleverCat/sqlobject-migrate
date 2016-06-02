d8e824c5533fe2c4c5215a9d630d6cc46d14e870
Evgeniy V. Kokovikhin <e.kokovikhin@co.wapstart.ru>
2016-06-02 16:25:16 +0300

    add time measure and transaction

--- sqlobjectmigrate/migrationBase.py
+++ sqlobjectmigrate/migrationBase.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+
 from version import Version
 from sqlobject import sqlhub
 import os
 
+
 class MigrationBase(object):
+
+    def __new__(cls):
+        migrationObject = super(MigrationBase, cls).__new__(cls)
+        migrationObject._connection = sqlhub.getConnection().transaction()
+        return migrationObject
 
     def getSameSqlFile(self, file):
         fileName = os.path.abspath(file)
@@ -14,23 +22,31 @@ class MigrationBase(object):
         with open(fileName, 'r') as f:
             read_data = f.read()
         if read_data:
-            sqlhub.getConnection().query(read_data)
+            self._connection.query(read_data)
 
     def getColumnDefinition(self, objectClass, columnName):
         return objectClass.sqlmeta.columnDefinitions[columnName].withClass(objectClass.sqlmeta.soClass)
 
     def addColumn(self, objectClass, columnName):
         print '--> add column %s to %s' % (columnName, objectClass.sqlmeta.table)
-        sqlhub.getConnection().addColumn(objectClass.sqlmeta.table, self.getColumnDefinition(objectClass, columnName))
+        self._connection.addColumn(objectClass.sqlmeta.table, self.getColumnDefinition(objectClass, columnName))
 
     def deleteColumn(self, objectClass, columnName):
         print '--> remove column %s from %s' % (columnName, objectClass.sqlmeta.table)
-        sqlhub.getConnection().delColumn(objectClass.sqlmeta, self.getColumnDefinition(objectClass, columnName))
+        self._connection.delColumn(objectClass.sqlmeta, self.getColumnDefinition(objectClass, columnName))
 
     def up(self):
         pass
 
     def upVersion(self, number):
-    	print '===== migration up - %s =====' % number
-        self.up()
-        Version(number = number)
+        try:
+            print '===== migration up - %s =====' % number
+            startTime = datetime.now()
+            self.up()
+            endTime = datetime.now()
+            print '==== for time - %s =====' % (endTime - startTime)
+            Version(number=number)
+            self._connection.commit(close=True)
+        except:
+            self._connection.rollback()
+            raise
